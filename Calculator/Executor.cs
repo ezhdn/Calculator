776601566d0618449f069965fa15f0434a5ba795
Eugene Zhadan <eugene.zhadan@gmail.com>
2016-02-20 21:04:38 +0500

    Extract primitive elements in expression

---
+++ Calculator/Executor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Executor
+    {
+        public static List<String> TokenizeExpression(string expression)
+        {
+            string parseExpression = expression.Replace(" ", "");
+
+            var reg = new Regex(@"[\+\-/\*]|[\d]+(\.[\d]+)?|[()]");
+
+            return reg.Matches(parseExpression).Cast<Match>().Select(x => x.Value).ToList();
+        }
+    }
+}
---
+++ UnitTests/Calculator.UnitTests/ExecutorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.UnitTests
+{
+    [TestFixture]
+    public class ExecutorTests
+    {
+        [TestCase("122+20*(43-(232/2323)/4)", 15)]
+        [TestCase("1+5", 3)]
+        [TestCase("12 2  +   20", 3)]
+        [TestCase("12 . 2  +   20", 3)]
+        public void TokenizeStingCountTest(String expression, int tokensCount)
+        {
+            var tokens = Executor.TokenizeExpression(expression);
+
+            Assert.AreEqual(tokens.Count, tokensCount);
+        }
+
+    }
+}
